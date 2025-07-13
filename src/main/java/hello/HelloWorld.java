54b098e0c737546f53712288d553226935f1a458
Nishad-Muhammed-CK <2021wb86317@wilp.bits-pilani.ac.in>
2025-07-13 17:59:04 +0530

    modified again

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,7 +5,7 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
-		  System.out.println("changes are made: " + currentTime);
+		  System.out.println("changes are made  : " + currentTime);
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
